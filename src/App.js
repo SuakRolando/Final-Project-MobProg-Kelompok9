b11aaf1eeb03e9f139c53750f97b81971ac23f78
SuakRolando <s21910305@student.unklab.ac.id>
2021-04-28 09:36:21 +0800

    membuat struktur atomic design di main project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Main Project</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
